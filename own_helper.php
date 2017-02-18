e86c153bad859301e45d08967ecd3309918c25a2
oyi77 <mbahkoe.pendekar@gmail.com>
2017-02-18 22:23:06 +0700

    update code for login and register

--- own_helper.php
+++ own_helper.php
@@ -13,8 +13,8 @@ if ( ! function_exists('cek'))
 {
     function cek($var = '')//buat kalo pengen ngecek variabel
     {
-    	$string = print_r($var, TRUE);
-    	$echo = "<pre>".$string."</pre>";
+        $string = print_r($var, TRUE);
+        $echo = "<pre>".$string."</pre>";
         return $echo;
     }   
 
@@ -24,7 +24,7 @@ if ( ! function_exists('alert'))
 {
     function alert($var = '')//buat bikin alert tapi lewat php
     {
-    	$echo = "<script>alert('".$var."')</script>";
+        $echo = "<script>alert('".$var."')</script>";
         return $echo;
     }   
 
@@ -34,7 +34,7 @@ if ( ! function_exists('login'))
 {
     function login($table = 'user', $username, $password, $enc = '')//buat login instant
     {
-    	$ci=& get_instance();
+        $ci=& get_instance();
         $ci->load->database(); 
 
         // $q = 
@@ -42,119 +42,265 @@ if ( ! function_exists('login'))
         $login = FALSE;
         $a = ambil_primary($table);
         if($enc == ''){
-        	$password = $password;
+            $password = $password;
+             $query = $ci->db->select('*')
+                        ->from($table)
+                        ->where('username', $username)
+                        ->or_where('email', $username)
+                        ->where('password', $password)
+                        ->get()->result_array();
+             $login = TRUE;
         }
         else if($enc == 'md5'){
-        	$password = md5($password);
+            $password = md5($password);
+             $query = $ci->db->select('*')
+                        ->from($table)
+                        ->where('username', $username)
+                        ->or_where('email', $username)
+                        ->where('password', $password)
+                        ->get()->result_array();
+            $login = TRUE;
         }
         else if($enc == 'password_hash'){
-        	$query = $ci->db->select('*')
-        				->from($table)
-        				->where('username', $username)
-        				->get()->result_array();
-        	$pass = $query[0]['password'];
-        	password_verify($password, $pass); 
-        	$login = TRUE;
+            $query = $ci->db->select('*')
+                        ->from($table)
+                        ->where('username', $username)
+                        ->or_where('email', $username)
+                        ->get()->result_array();
+            $pass = $query[0]['password'];
+            password_verify($password, $pass); 
+            $login = TRUE;
         }
 
-		if($login == TRUE){
-		unset($query[0]['password']);
-		}        
-		else {
-
-        $query = $ci->db->select('*')
-        				->from($table)
-        				->where('username', $username)
-        				->where('password', $password)
-        				->get()->result_array();
-        $login = TRUE;
-		}
-
-
-
+        // if($login == TRUE){
+        // unset($query[0]['password']);
+        // }    
 
         set_sesi($a, $query[0][$a]);
 
         
-    	return $query;
+        return $query;
     }
 }
 
 if ( ! function_exists('ambil_sesi'))//buat pengambilan session
 {
     function ambil_sesi($nama_sesi = ''){
-    	 $ci=& get_instance();
+         $ci=& get_instance();
          $ci->load->library('session');
 
-	     return $ci->session->userdata($nama_sesi);
-    	    }
+         return $ci->session->userdata($nama_sesi);
+            }
 }
 
 if ( ! function_exists('register'))//buat register instant tapi belum sempurna karena kurang instan
 {
-    function register($nama_tabel = '', $data){
-    	 $ci=& get_instance();
+    function register($nama_tabel = '', $data, $enc = ''){
+         $ci=& get_instance();
          $ci->load->database();
-	     return $ci->db->insert($nama_tabel, $data);
-    	    }
+
+         $query = $ci->db->select('*')
+                        ->from($nama_tabel)
+                        ->where('username', $data['username'])
+                        ->or_where('email', $data['email'])
+                        ->get()->result_array();
+        $num = count($query);
+
+         if($enc == ''){
+            if(array_key_exists('confirm_password', $data) == true){
+                if($data['password'] == $data['confirm_password']){
+                    $status = "password cocok";
+                    unset($data['confirm_password']);
+                }
+                else if($data['pass'] == $data['confirm_password']){
+                    $status = "password cocok";
+                    unset($data['confirm_password']);
+                }
+                else {
+                    $status = "password salah";
+                }
+            }
+            else if(array_key_exists('password_confirm', $data) == true) {
+                if($data['password'] == $data['password_confirm']){
+                    $status = "password cocok";
+                    unset($data['password_confirm']);
+                }
+                else if($data['pass'] == $data['password_confirm']){
+                    $status = "password cocok";
+                    unset($data['password_confirm']);
+                }
+                else {
+                    $status = "password salah";
+                }
+            }
+            else {
+                $status = "tidak ada konfirmasi password";
+            }
+             if($num == 0){
+                $ci->db->insert($nama_tabel, $data);
+             }
+             else{
+                $status = "Data Exist!";
+             }
+         }
+         else if($enc == 'md5'){
+            if(array_key_exists('confirm_password', $data) == true){
+                if($data['password'] == $data['confirm_password']){
+                    $status = "password cocok";
+                    unset($data['confirm_password']);
+                }
+                else if($data['pass'] == $data['confirm_password']){
+                    $status = "password cocok";
+                    unset($data['confirm_password']);
+                }
+                else {
+                    $status = "password salah";
+                }
+            }
+            else if(array_key_exists('password_confirm', $data) == true) {
+                if($data['password'] == $data['password_confirm']){
+                    $status = "password cocok";
+                    unset($data['password_confirm']);
+                }
+                else if($data['pass'] == $data['password_confirm']){
+                    $status = "password cocok";
+                    unset($data['password_confirm']);
+                }
+                else {
+                    $status = "password salah";
+                }
+            }
+            else {
+                $status = "tidak ada konfirmasi password";
+            }
+            if(array_key_exists('pass', $data) == true){
+                $pass = array('pass' => md5($data['pass']));
+                unset($data['pass']);
+                $data = array_merge($data, $pass);
+                }
+            else if(array_key_exists('password', $data) == true){
+                $pass = array('password' => md5($data['password']));
+                unset($data['password']);
+                $data = array_merge($data, $pass);
+                }
+             if($num == 0){
+                $ci->db->insert($nama_tabel, $data);
+             }
+             else{
+                $status = "Data Exist!";
+             }
+         }
+         else if($enc == 'password_hash'){
+            $options = [
+                        'cost' => 11,
+                        ];
+            if(array_key_exists('confirm_password', $data) == true){
+                if($data['password'] == $data['confirm_password']){
+                    $status = "password cocok";
+                    unset($data['confirm_password']);
+                }
+                else if($data['pass'] == $data['confirm_password']){
+                    $status = "password cocok";
+                    unset($data['confirm_password']);
+                }
+                else {
+                    $status = "password salah";
+                }
+            }
+            else if(array_key_exists('password_confirm', $data) == true) {
+                if($data['password'] == $data['password_confirm']){
+                    $status = "password cocok";
+                    unset($data['password_confirm']);
+                }
+                else if($data['pass'] == $data['password_confirm']){
+                    $status = "password cocok";
+                    unset($data['password_confirm']);
+                }
+                else {
+                    $status = "password salah";
+                }
+            }
+            else {
+                $status = "tidak ada konfirmasi password";
+            }
+            if(array_key_exists('pass', $data) == true){
+                $pass = array('pass' => password_hash($data['pass'], PASSWORD_BCRYPT, $options));
+                unset($data['pass']);
+                $data = array_merge($data, $pass);
+                }
+            else if(array_key_exists('password', $data) == true){
+                $pass = array('password' => password_hash($data['password'], PASSWORD_BCRYPT, $options));
+                unset($data['password']);
+                $data = array_merge($data, $pass);
+                }
+             if($num == 0){
+                $ci->db->insert($nama_tabel, $data);
+             }
+             else{
+                $status = "Data Exist!";
+             }
+         }
+
+         return $status;
+            }
 }
 
 if ( ! function_exists('set_sesi'))//setting session
 {
     function set_sesi($nama_sesi = '', $data){
-    	 $ci=& get_instance();
+         $ci=& get_instance();
          $ci->load->library('session');
 
-	     return $ci->session->set_userdata($nama_sesi, $data);
-    	    }
+         return $ci->session->set_userdata($nama_sesi, $data);
+            }
 }
 
 if ( ! function_exists('hapus_sesi'))//deleting session 
 {
     function hapus_sesi($nama_sesi = ''){
-    	 $ci=& get_instance();
+         $ci=& get_instance();
          $ci->load->library('session');
 
-         if ($nama_sesi = ''){
-         	$ci->session->sess_destroy();
-         	$echo = "session destroyed!";
+         if ($nama_sesi == ''){
+            $ci->session->sess_destroy();
+            $echo = "session destroyed!";
          }
 
          else {
-         	$ci->session->unset_userdata($nama_sesi);
-         	$echo = "session unsetted!";
+            $ci->session->unset_userdata($nama_sesi);
+            $echo = "session unsetted!";
          }
 
-	     return $echo;
-    	    }
+         return $echo;
+            }
 }
 
 if ( ! function_exists('form_register'))//buat bikin form register instant
 {
-	function form_register($table, $action){
+    function form_register($table, $action){
 
-	}
+    }
 }
 
 if ( ! function_exists('form_login'))//buat bikin form register instant
 {
-	function form_login($table, $action){
+    function form_login($table, $action){
 
-	}
+    }
 }
 
 if ( ! function_exists('ambil_primary'))
 {
-   	function ambil_primary($table){
-   		$ci=& get_instance();
+    function ambil_primary($table){
+        $ci=& get_instance();
         $ci->load->database(); 
-   		$val = $ci->db->field_data($table);
+        $val = $ci->db->field_data($table);
 
         foreach ($val as $a) {
-        	if($a->primary_key == 1){
-        		return $a->name;
-        	}
+            if($a->primary_key == 1){
+                return $a->name;
+            }
         }
-   	}
+    }
 
 }
